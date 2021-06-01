b963cf2992906c7a9695b9857c78de3234c21f5b
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2021-06-02 08:10:19 +1000

    Feat: Add twitter auth

---
+++ client/src/components/Twitter.js
@@ -0,0 +1,16 @@
+import React from "react";
+import TwitterLogin from "react-twitter-login";
+
+export default (props) => {
+  const authHandler = (err, data) => {
+    console.log(err, data);
+  };
+
+  return (
+    <TwitterLogin
+      authCallback={authHandler}
+      consumerKey={CONSUMER_KEY}
+      consumerSecret={CONSUMER_SECRET}
+    />
+  );
+};
